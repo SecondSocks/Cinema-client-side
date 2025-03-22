617b7540d6080b68f5d6ba9326b23c02a772c227
SecondSocks <159525526+SecondSocks@users.noreply.github.com>
2025-03-22 15:08:34 +0700

    Init and configured styles

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+export default function page() {
+	return <div className='w-screen h-screen flex flex-col'>
+    <button className='btn-primary'>Click me</button>
+    <a href='#' className='text-link text-black text-shadow'>Link</a>
+  </div>
+}
